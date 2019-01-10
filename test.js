4efbf276c1a481753776bcc96b5f530e77b9616e
Yachint <yachint.yadav@gmail.com>
2019-01-10 22:32:02 +0530

    Fourth Commit

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const Blockchain = require('./lab2');
 const bitcoin = new Blockchain();
+var test = 50;
 bitcoin.createNewBlock('EIUF4343BKN43J4334K');
 bitcoin.createNewBlock('EIUF4343BeuieKN43J4334K');
 console.log(bitcoin);
