683508bfea50ac89216b9da353d699bb6ce6c013
Yachint <yachint.yadav@gmail.com>
2019-01-10 22:33:21 +0530

    Last Practice Commit

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const Blockchain = require('./lab2');
 const bitcoin = new Blockchain();
-var test = 50;
 bitcoin.createNewBlock('EIUF4343BKN43J4334K');
 bitcoin.createNewBlock('EIUF4343BeuieKN43J4334K');
 console.log(bitcoin);
